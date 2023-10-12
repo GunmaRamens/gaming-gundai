fed7e278cfc1c1c2102826c74e26c669d8caaae6
hayao <hayao@fascode.net>
2023-10-12 15:04:59 +0900

    Fix: Warn pretter as eslint rule

--- src/sso.ts
+++ src/sso.ts
@@ -5,7 +5,6 @@ import addClass from "./utils/addClass";
 import changeQueryInnerHTML from "./utils/changeQueryInnerHTML";
 
 window.addEventListener("load", () => {
-    
     addClass([".header_column", ".input_form", ".input_column"], ["rainbow"]);
 
     changeQueryInnerHTML([".product"], "群馬大学ゲーミングサインオンシステム");
