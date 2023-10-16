8f4fab5eb5a0cb135bed6fa344a9347c66e4264a
hayao <hayao@fascode.net>
2023-10-16 12:32:33 +0900

    Add: Re-add file

---
+++ src/utils/RunFuncIfEnabled.ts
@@ -0,0 +1,8 @@
+import getIsEnabled from "./GetIsEnabled";
+
+export default async function RunFuncIfEnabled(func: () => void) {
+    const isEnable = await getIsEnabled();
+    if (isEnable) {
+        func();
+    }
+}
