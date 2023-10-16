efdf4da1fc8e4fbdb36b592c7c7390753757a329
hayao <hayao@fascode.net>
2023-10-16 12:29:51 +0900

    Remove: Old files

--- src/utils/changeQueryInnerHTML.ts
+++
@@ -1,8 +0,0 @@
-export default function ChangeQueryInnerHTML(queries: string[], innerHTML: string) {
-    queries.forEach((query) => {
-        const elements = document.querySelectorAll(query);
-        elements.forEach((element) => {
-            element.innerHTML = innerHTML;
-        });
-    });
-}
--- src/utils/loadGamingWebsite.ts
+++
@@ -1,7 +0,0 @@
-import GundaiWebSite from "../class/GundaiWebsite";
-import RunFuncIfEnabled from "./RunFuncIfEnabled";
-
-// ウィンドウが読み込まれたらGundaiWebsiteのEnableRainbowを実行する
-export default function LoadGamingWebsite(website: GundaiWebSite) {
-    window.addEventListener("load", async () => await RunFuncIfEnabled(website.EnableRainbow));
-}
