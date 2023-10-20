ca7e227a502b4660e142ca1a0a5cac20e4795e2a
hayao <hayao@fascode.net>
2023-10-20 15:35:34 +0900

    Fix: No use useEffect

--- src/popup/components/SwitchItem.tsx
+++ src/popup/components/SwitchItem.tsx
@@ -1,21 +1,27 @@
 //import { useEffect } from "react";
 
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 
 import { WebSites } from "../../class";
 import IsTrue from "../../utils/IsTrue";
-import { FrontConfigs } from "../config";
+import { FrontConfig, FrontConfigs } from "../config";
 
 export default function SwitchItem() {
     const [enabled, setEnabled] = useState({} as { [key: string]: string });
 
-    useEffect(() => {
-        console.log("Render SwitchItem");
-        Object.keys(WebSites).forEach(async (key) => {
-            const value = (await WebSites[key].ReadStorage("enabled")) as string;
-            setEnabled((enabled) => ({ ...enabled, [key]: value }));
-        });
+    console.log("Render SwitchItem");
+    Object.keys(WebSites).forEach(async (key) => {
+        const value = (await WebSites[key].ReadStorage("enabled")) as string;
+        setEnabled((enabled) => ({ ...enabled, [key]: value }));
     });
+
+    const genericChangeHandle = useCallback((config: FrontConfig) => {
+        return (e: React.ChangeEvent<HTMLInputElement>) => {
+            setEnabled((enabled) => ({ ...enabled, [config.id]: e.target.checked.toString() }));
+            WebSites[config.id].WriteStorage("enabled", e.target.checked.toString());
+            chrome.runtime.sendMessage("reload");
+        };
+    }, []);
 
     return FrontConfigs.map((config) => {
         return (
@@ -24,14 +30,8 @@ export default function SwitchItem() {
                     <input
                         type="checkbox"
                         className=" daisy-toggle daisy-toggle-info"
-                        onChange={(e) => {
-                            setEnabled((enabled) => ({ ...enabled, [config.id]: e.target.checked.toString() }));
-                            WebSites[config.id].WriteStorage("enabled", e.target.checked.toString());
-                            chrome.runtime.sendMessage("reload");
-                        }}
-                        checked={(function () {
-                            return IsTrue(enabled[config.id]);
-                        })()}
+                        onChange={genericChangeHandle(config)}
+                        checked={IsTrue(enabled[config.id])}
                     />
                 </div>
                 <div className="w-1/2">
