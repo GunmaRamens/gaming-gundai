3fe262f85918005830cd2972f9b6f3db6d5049ac
hayao <hayao@fascode.net>
2023-10-24 14:27:11 +0900

    Update: Hide popup switch with options page

--- src/popup/components/Main.tsx
+++ src/popup/components/Main.tsx
@@ -1,13 +1,25 @@
-import { DetailedHTMLProps } from "react";
+import { DetailedHTMLProps, useState } from "react";
 
 import { Switches } from "../../components/Switches";
 
 type MainProps = DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
 
 export default function Main(props: MainProps) {
+    const [showSwitchClass, setShowSwitchClass] = useState(true);
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (e) => {
+        const url = e[0].url;
+        if (url == "chrome-extension://kgmnkhekdmbebohfheckhcmdfofkjmfp/options.html") {
+            setShowSwitchClass(false);
+        }
+    });
+
+    const switches = <Switches className="w-full justify-center child:w-1/2 child:text-center" />;
+    const hiddenMsg = <p className="text-center text-lg">設定画面を表示中はポップアップを利用できません</p>;
+
     return (
-        <main {...props}>
-            <Switches className="w-full justify-center child:w-1/2 child:text-center" />
-        </main>
+        <>
+            <main {...props}>{showSwitchClass ? switches : hiddenMsg}</main>
+        </>
     );
 }
