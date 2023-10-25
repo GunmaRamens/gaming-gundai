7cff0fc784a1456aeed16c360ea1cf1e6ba62683
hayao <hayao@fascode.net>
2023-10-26 05:31:16 +0900

    Fix: Use runtime API to get extension ID

--- src/popup/components/Main.tsx
+++ src/popup/components/Main.tsx
@@ -1,4 +1,4 @@
-import { DetailedHTMLProps, useState } from "react";
+import { DetailedHTMLProps, useEffect, useState } from "react";
 
 import { Switches } from "../../components/Switches";
 
@@ -7,12 +7,16 @@ type MainProps = DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElemen
 export default function Main(props: MainProps) {
     const [showSwitchClass, setShowSwitchClass] = useState(true);
 
-    chrome.tabs.query({ active: true, currentWindow: true }, (e) => {
-        const url = e[0].url;
-        if (url == "chrome-extension://kgmnkhekdmbebohfheckhcmdfofkjmfp/options.html") {
-            setShowSwitchClass(false);
-        }
-    });
+    useEffect(
+        () =>
+            chrome.tabs.query({ active: true, currentWindow: true }, (e) => {
+                const url = e[0].url;
+                if (url == `chrome-extension://${chrome.runtime.id}/options.html`) {
+                    setShowSwitchClass(false);
+                }
+            }),
+        [],
+    );
 
     const switches = <Switches className="w-full justify-center child:w-1/2 child:text-center" />;
     const hiddenMsg = <p className="text-center text-lg">設定画面を表示中はポップアップを利用できません</p>;
