8391e68be7d3bde2f7a6e9a66e6cdb66611b4230
hayao <hayao@fascode.net>
2023-10-26 05:44:30 +0900

    Update: Add buttons on popup

--- src/popup/components/Footer.tsx
+++ src/popup/components/Footer.tsx
@@ -1,3 +1,14 @@
+import OpenOptions from "../../utils/OpenOptions";
+
 export default function Footer() {
-    return <></>;
+    return (
+        <div className="my-5 flex justify-center child:m-2">
+            <button className="btn" onClick={() => OpenOptions("about")}>
+                この拡張機能について
+            </button>
+            <button className="btn" onClick={() => OpenOptions("home")}>
+                設定
+            </button>
+        </div>
+    );
 }
