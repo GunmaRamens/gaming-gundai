f1fd9a651ab1ce1d6ced78bdcd00fd3a709e6a6e
hayao <hayao@fascode.net>
2023-11-02 14:23:29 +0900

    Update: Add advertisement

--- src/popup/components/Header.tsx
+++ src/popup/components/Header.tsx
@@ -1,6 +1,7 @@
 import "@/styles/gaming.scss";
 
 import { DetailedHTMLProps } from "react";
+import { Collapse } from "react-daisyui";
 
 type HeaderProps = DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
 
@@ -11,10 +12,24 @@ export default function Header(props: HeaderProps) {
                 <p className="rainbow-text text-2xl">Gaming Gunma University</p>
             </div>
 
-            <div className="my-4">
-                <p>荒牧祭で「長谷川とら」で出店します。</p>
-                <p>「ゲーミング群馬大学入れました」で割引します。</p>
-            </div>
+            <Advertisement />
         </header>
+    );
+}
+
+function Advertisement() {
+    return (
+        <>
+            <Collapse checkbox icon="arrow" className="my-5 bg-neutral-focus">
+                <Collapse.Title className="flex text-lg">
+                    <span className="rainbow-text font-serif italic">NEW</span>
+                    <span className="grow text-center">荒牧祭について</span>
+                </Collapse.Title>
+                <Collapse.Content>
+                    <p>荒牧祭で「長谷川とら」で出店します。</p>
+                    <p>「ゲーミング群馬大学入れました」で割引します。</p>
+                </Collapse.Content>
+            </Collapse>
+        </>
     );
 }
