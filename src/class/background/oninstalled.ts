fa0731224e620d37f7a4e7798f9ea8aeacd59bfe
hayao <hayao@fascode.net>
2023-11-26 01:45:02 +0900

    Update: initialize db on the first installation

--- src/class/background/oninstalled.ts
+++ src/class/background/oninstalled.ts
@@ -1,8 +1,14 @@
 import StorageTool from "@/class/Storage/browser";
+import { WebsitesList } from "@/data/websites";
 import isTrue from "@/utils/isTrue";
 import OpenOptions from "@/utils/openOptions";
 
 export default async function onInstalled() {
+    await setInstalled();
+    await initilizeWebsiteDb();
+}
+
+const setInstalled = async () => {
     const storage = new StorageTool("other");
     const isInstalled = await storage.get("installed");
     //console.log(isInstalled);
@@ -10,4 +16,14 @@ export default async function onInstalled() {
     OpenOptions("thanks")?.then(() => {
         storage.set("installed", "true");
     });
-}
+};
+
+const initilizeWebsiteDb = async () => {
+    const storage = new StorageTool("other");
+    const isInstalled = await storage.get("installed");
+    //console.log(isInstalled);
+    if (isInstalled != undefined && isTrue(isInstalled)) return;
+    WebsitesList.forEach(async (website) => {
+        website.class.initilizeWebsiteDb();
+    });
+};
