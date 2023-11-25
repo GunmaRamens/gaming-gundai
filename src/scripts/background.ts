4f0baaeb91b2f50f116a2092210ace8fc4b994bf
hayao <hayao@fascode.net>
2023-11-26 01:41:16 +0900

    Fix: await onInstalled

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -4,7 +4,7 @@ import * as Background from "@/class/background";
 
 // インストール時の動作
 browser.runtime.onInstalled.addListener(async () => {
-    Background.onInstalled();
+    await Background.onInstalled();
 });
 
 browser.storage.onChanged.addListener(Background.onStorageChanged);
